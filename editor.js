3bf0dfb723ded2bbf7a071ba707ced30d8384907
otomo <naohiro_otomo@cbnext.co.jp>
2019-01-18 12:46:43 +0900

    エディタエリアが表示されるように

--- editor.js
+++ editor.js
@@ -1,5 +1,1 @@
-function onLoad(){
-  var editor = ace.edit("input_area");
-  editor.setTheme("ace/theme/monokai");
-  editor.getSession().setMode("ace/mode/html");
-}
+var editor = ace.edit("input_area");
