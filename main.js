6b64eabdc31c7b599757b8da5845b4e037965fa4
monao <nohr@outlook.jp>
2019-01-22 00:45:53 +0900

    メニューバー修正

--- main.js
+++ main.js
@@ -1,9 +1,86 @@
 const electron = require('electron');
 const app = electron.app;
+const Menu = electron.Menu
 const BrowserWindow = electron.BrowserWindow;
+const fs = require("fs")
 
 let mainWindow = null;
 
+//メニューバーテンプレート作成
+const template = [
+  {
+    label: 'File',
+    submenu: [
+      { 
+        label: 'Open'
+
+      },
+      { 
+        label: 'Save'
+
+      },
+      { type: 'separator' },
+      { role: 'quit' }
+    ]
+  },
+  {
+    label: 'Edit',
+    submenu: [
+      { role: 'undo' },
+      { role: 'redo' },
+      { type: 'separator' },
+      { role: 'cut' },
+      { role: 'copy' },
+      { role: 'paste' },
+      { role: 'pasteandmatchstyle' },
+      { role: 'delete' },
+      { role: 'selectall' }
+    ]
+  },
+  {
+    label: 'View',
+    submenu: [
+      { role: 'reload' },
+      { role: 'forcereload' },
+      { role: 'toggledevtools' },
+      { type: 'separator' },
+      { role: 'resetzoom' },
+      { role: 'zoomin' },
+      { role: 'zoomout' },
+      { type: 'separator' },
+      { role: 'togglefullscreen' },
+      {
+        label: 'Background',
+        submenu: [
+          {
+            label: '明るいやつ',
+            type: 'checkbox',
+            type: 'checked',
+            click () {exports}
+          }
+        ]
+      }
+    ]
+  },
+  {
+    role: 'window',
+    submenu: [
+      { role: 'minimize' },
+      { role: 'close' }
+    ]
+  },
+  {
+    role: 'help',
+    submenu: [
+      {
+        label: 'Learn More',
+        click () { require('electron').shell.openExternal('https://electronjs.org') }
+      }
+    ]
+  }
+]
+
+// 全てのウィンドウが閉じたら終了
 app.on('window-all-closed', function() {
   if (process.platform !== 'darwin') {
     app.quit();
@@ -20,3 +97,8 @@ app.on('ready', function() {
     mainWindow = null;
   });
 });
+
+
+//メニューバー生成
+const menu = Menu.buildFromTemplate(template);
+Menu.setApplicationMenu(menu);
